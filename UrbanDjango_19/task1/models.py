0a0846acc3c0cb6a0dcad5b39cbf335bafbe5b92
Ekaterina <ekaterina.ukladnikova@mail.ru>
2025-09-30 20:19:13 +0300

    Добавлено выполненное задание: QuerySet запросы в базу данных

--- UrbanDjango_19/task1/models.py
+++ UrbanDjango_19/task1/models.py
@@ -3,6 +3,45 @@
 # Задача "Модели игрового магазина":
 # Студентка: Укладникова Екатерина
 # Дата: 24.09.2025г.
+#------------------------------
+# Домашнее задание по теме "QuerySet запросы в
+# базу данных"
+# Цель: сделать первые записи в БД при помощи QuerySet запросов,
+# закрепив знания о них.
+# Задача "Я буду устанавливать все игры!":
+# список QuerySet запросов в порядке вызовов, которые
+# я использовала для внесения изменений в БД
+# (Команда для запуска в терминале QuerySet запросов: python manage.py shell):
+
+# from task1.models import Buyer
+
+# Добавляю покупателей:
+# Buyer.objects.create(name='Ilya', balance=1500.05, age=24)
+# Buyer.objects.create(name='Terminator2000', balance=42.15, age=52)
+# Buyer.objects.create(name='Ubivator432', balance=0.5, age=16)
+
+# from task1.models import Game
+
+# Добавляю игры в каталог:
+# Game.objects.create(title='Cyberpunk 2077', cost=31, size=46.2, description='Game of year', age_limited=1)
+# Game.objects.create(title='Mario', cost=5, size=0.5, description='Old Game', age_limited=0)
+# Game.objects.create(title='Hitman', cost=12, size=36.6, description='Who kills Mark?', age_limited=1)
+
+# Продаю игры с учетом возраста покупателей:
+# first_buyer = Buyer.objects.get(age__lt=18)
+# second_buyer, third_buyer = Buyer.objects.filter(age__gte=18)
+# Game.objects.get(id=1).buyer.set((second_buyer, third_buyer))
+# Game.objects.get(id=2).buyer.set([third_buyer])
+# Game.objects.get(id=3).buyer.set((second_buyer, first_buyer, third_buyer))
+
+# Выводим список покупателей
+# buyer = Buyer.objects.all()
+# print(buyer)
+
+# Выводим список игр
+# game = Game.objects.all()
+# print(game)
+
 
 from decimal import Decimal
 from django.db import models
