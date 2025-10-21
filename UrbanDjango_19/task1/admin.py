069119162e407ccef929a422fbde32d95994d6aa
Ekaterina <ekaterina.ukladnikova@mail.ru>
2025-10-21 21:47:11 +0300

    Сайт администрирования

--- UrbanDjango_19/task1/admin.py
+++ UrbanDjango_19/task1/admin.py
@@ -1,8 +1,36 @@
+# Домашнее задание по теме "Сайт администрирования".
+
 from django.contrib import admin
+from task1.models import Buyer, Game
 
 # Register your models here.
 
-from task1.models import Buyer, Game
+# admin.site.register(Buyer)
+# admin.site.register(Game)
 
-admin.site.register(Buyer)
-admin.site.register(Game)
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size',)
+    # fields = [('title', 'cost'), 'description', 'size',]
+    search_fields = ('title',)
+    list_filter = ('cost', 'size',)
+    fieldsets = (
+        ('info', {
+            'fields':
+                ('title', 'cost')
+        }),
+        ('footer', {
+            'fields':
+                ('description', 'size')
+        }),
+    )
+    list_max_show_all = 20
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age',)
+    fields = [('name', 'balance'), 'age',]
+    search_fields = ('name',)
+    list_filter = ('balance', 'age',)
+    readonly_fields = ('balance',)
+    list_max_show_all = 30
