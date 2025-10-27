eaaa1597997b64535d93277e61d7912761d0de76
Ekaterina <ekaterina.ukladnikova@mail.ru>
2025-10-27 17:48:56 +0300

    -

--- UrbanDjango_19/UrbanDjango_19/settings.py
+++ UrbanDjango_19/UrbanDjango_19/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'ukladnikovaekaterina',
     'USER': 'postgres',
-    'PASSWORD': 'kate1969',
+    'PASSWORD': '',
     'HOST': '127.0.0.1',
     'PORT': '5432',
     }
